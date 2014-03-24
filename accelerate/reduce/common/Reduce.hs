dab09e792237d5d0637d225ee77ad29a6a308ea7
Ryan Newton <rrnewton@gmail.com>
2014-03-24 14:08:32 -0400

    HSBencher update

--- accelerate/reduce/common/Reduce.hs
+++ accelerate/reduce/common/Reduce.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP, NamedFieldPuns #-}
 
 -- | A simple reduction microbenchmark.
 
@@ -7,6 +8,7 @@ import System.Random.MWC
 import Data.Array.IArray     as IArray
 import Data.Array.Accelerate as Acc
 import Prelude               as P
+import Random (randomUArrayR, convertUArray)
 
 #ifdef ACCBACKEND
 import qualified ACCBACKEND as Bkend
@@ -17,46 +19,23 @@ import qualified ACCBACKEND as Bkend
 
 import Data.Array.Accelerate as A 
 import Data.Array.Accelerate as Acc
--- import Data.Array.Accelerate.BackendClass (runTimed, AccTiming(..), Backend(..))
+import Data.Array.Accelerate.BackendClass (runTimed, AccTiming(..), Backend(..))
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 
 import Control.Exception (evaluate)
 import System.Environment (getArgs)
 --------------------------------------------------------------------------------
 
-{-
--- SAXPY
--- -----
-foldem :: Vector Float -> Acc (Scalar Float)
-foldem alpha xs ys
-  = let
-      xs' = use xs
-      ys' = use ys
-    in
-    Acc.zipWith (\x y -> constant alpha * x + y) xs' ys'
 
-saxpyRef :: Float -> UArray Int Float -> UArray Int Float -> UArray Int Float
-saxpyRef alpha xs ys
-  = listArray (bounds xs) [alpha * x + y | x <- elems xs | y <- elems ys]
-
--- Main
--- ----
-
-runrun :: Int -> IO (() -> UArray Int Float, () -> Acc (Vector Float))
-runrun nelements = withSystemRandom $ \gen -> do
+-- runrun :: Int -> IO (() -> UArray Int Float, () -> Acc (Vector Float))
+-- mkArr :: Int -> IO (UArray Int Float)
+mkArr :: Int -> IO (Vector Float)
+mkArr nelements = withSystemRandom $ \gen -> do
   v1    <- randomUArrayR (-1,1) gen nelements
-  v2    <- randomUArrayR (-1,1) gen nelements
   v1'   <- convertUArray v1
-  v2'   <- convertUArray v2
-  alpha <- uniform gen
-  --
-  return (run_ref alpha v1 v2, run_acc alpha v1' v2')
-  where
-    {-# NOINLINE run_ref #-}
-    run_ref alpha xs ys () = saxpyRef alpha xs ys
-    run_acc alpha xs ys () = saxpyAcc alpha xs ys
--}
-
+  return v1'
+--  alpha <- uniform gen
+--  run_acc alpha xs ys () = saxpyAcc alpha xs ys
 
 --  
 reduce :: Acc (Vector Float) -> Acc (Scalar Float)
@@ -68,16 +47,27 @@ main = do args <- getArgs
           let inputSize = case args of
                             []   -> 100
                             [sz] -> read sz
---           (_,go) <- (runrun inputSize)
+          
+          (t0,input) <- timeit$ mkArr inputSize
+
+          let go = reduce (A.use input)
 
           tBegin <- getCurrentTime
-{-
-          (times,_output) <- runTimed Bkend.defaultBackend Nothing Bkend.defaultTrafoConfig (go ())
+          (times,_output) <- runTimed Bkend.defaultBackend Nothing Bkend.defaultTrafoConfig go
           tEnd   <- getCurrentTime
           let AccTiming{compileTime,runTime,copyTime} = times
           putStrLn$ "  All timing: "++ show times
           putStrLn$ "  Total time for runTimed "++ show (diffUTCTime tEnd tBegin)
           putStrLn$ "JITTIME: "++ show compileTime
           putStrLn$ "SELFTIMED: "++ show (runTime + copyTime)
--}
+
+          putStrLn "Done with benchmark."
           return ()
+
+-- timeit :: Fractional t => IO a -> (a,t)
+timeit :: IO a -> IO (Double,a)
+timeit act = do 
+   tBegin <- getCurrentTime
+   x <- act
+   tEnd   <- getCurrentTime
+   return $! (fromRational $ toRational $ diffUTCTime tEnd tBegin, x)
