a6d37e030f4e77e3d87f51b48320bd6b31fc1faa
Ryan Newton <rrnewton@gmail.com>
2014-07-02 16:19:08 -0400

    Add fusion upload

--- OpenAcc/run-openacc-benchmark.sh
+++ OpenAcc/run-openacc-benchmark.sh
@@ -12,4 +12,18 @@ fi
 cd $DIR
 $CABAL install --disable-documentation --bindir=. --program-suffix=.exe
 
-./run-openacc-benchmark.exe
+# Fusion table upload
+# ============================================================
+# Parfunc account, registered app in api console:
+CID=905767673358.apps.googleusercontent.com
+SEC=2a2H57dBggubW1_rqglC7jtK
+# ArrayDSL/Accelerate-multidev table docID:
+# TABID=1E17ssTkVafPYjzPjO9m1uOqlq8Cz2T9D48PQo7s
+# https://www.google.com/fusiontables/DataSource?docid=1E17ssTkVafPYjzPjO9m1uOqlq8Cz2T9D48PQo7s
+TABLENAME=Array-DSL-bench-results
+# ============================================================
+
+TRIALS=3
+
+# Now run it:
+./run-openacc-benchmark.exe --keepgoing --trials=$TRIALS --fusion-upload --name=$TABLENAME --clientid=$CID --clientsecret=$SEC $*
