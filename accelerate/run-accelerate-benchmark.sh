a7dbf9a3c27dbd4f11176ea9928559ac04200e30
Ryan Newton <rrnewton@gmail.com>
2014-07-07 20:47:29 -0400

    Update hsbencher, add more chatter to accelerate benchmark script

--- accelerate/run-accelerate-benchmark.sh
+++ accelerate/run-accelerate-benchmark.sh
@@ -14,6 +14,9 @@ which icc || echo ok
 # Adding this for Delta on Futuregrid, but it should be harmless elsewhere.
 module add cuda/5.5 || echo "Ok that that didnt work."
 
+which -a nvcc  || echo ok
+nvcc --version || echo ok
+
 # The first argument to this script is usually the root directory for the repo.
 if [ -d "$1" ]; then 
   HERE=$1/accelerate
